a82992696dc39154258d1fa706389cd9f6a4c73a
Neil de Carteret <git@lumphammer.net>
2025-06-01 09:48:32 +0100

    tidy ability schemas

--- src/module/items/createAbilitySchema.ts
+++ src/module/items/createAbilitySchema.ts
@@ -7,26 +7,80 @@ import ArrayField = foundry.data.fields.ArrayField;
 import SchemaField = foundry.data.fields.SchemaField;
 
 export const createAbilitySchema = () => ({
-  // rating: number;
-  rating: new NumberField({ nullable: false, required: true, initial: 0 }),
-  // pool: number;
-  pool: new NumberField({ nullable: false, required: true, initial: 0 }),
-  // min: number;
-  min: new NumberField({ nullable: false, required: true, initial: 0 }),
-  // max: number;
-  max: new NumberField({ nullable: false, required: true, initial: 0 }),
-  // occupational: boolean;
-  occupational: new BooleanField({ nullable: false, required: true }),
-  // hasSpecialities: boolean;
-  hasSpecialities: new BooleanField({ nullable: false, required: true }),
-  // specialitiesMode: SpecialitiesMode;
-  specialitiesMode: new StringField({
+  allowPoolToExceedRating: new BooleanField({
     nullable: false,
     required: true,
-    choices: ["one", "twoThreeFour"],
-    initial: "one",
   }),
-  // specialities: string[];
+  boost: new BooleanField({
+    nullable: false,
+    required: true,
+  }),
+  categoryId: new StringField({
+    nullable: false,
+    required: true,
+  }),
+  excludeFromGeneralRefresh: new BooleanField({
+    nullable: false,
+    required: true,
+  }),
+  hasSpecialities: new BooleanField({
+    nullable: false,
+    required: true,
+  }),
+  max: new NumberField({
+    nullable: false,
+    required: true,
+    initial: 8,
+  }),
+  min: new NumberField({
+    nullable: false,
+    required: true,
+    initial: 0,
+  }),
+  notes: createNotesWithFormatField(),
+  occupational: new BooleanField({
+    nullable: false,
+    required: true,
+  }),
+  pool: new NumberField({
+    nullable: false,
+    required: true,
+    initial: 0,
+  }),
+  rating: new NumberField({
+    nullable: false,
+    required: true,
+    initial: 0,
+  }),
+  refreshesDaily: new BooleanField({
+    nullable: false,
+    required: true,
+  }),
+  showTracker: new BooleanField({
+    nullable: false,
+    required: true,
+  }),
+  situationalModifiers: new ArrayField(
+    new SchemaField({
+      id: new StringField({
+        nullable: false,
+        required: true,
+      }),
+      situation: new StringField({
+        nullable: false,
+        required: true,
+      }),
+      modifier: new NumberField({
+        nullable: false,
+        required: true,
+      }),
+    }),
+    {
+      nullable: false,
+      required: true,
+      initial: [],
+    },
+  ),
   specialities: new ArrayField(
     new StringField({
       nullable: false,
@@ -37,35 +91,27 @@ export const createAbilitySchema = () => ({
       required: true,
     },
   ),
-  // showTracker: boolean;
-  showTracker: new BooleanField({ nullable: false, required: true }),
-  // boost: boolean;
-  boost: new BooleanField({ nullable: false, required: true }),
-  // categoryId: string;
-  categoryId: new StringField({ nullable: false, required: true }),
-  // excludeFromGeneralRefresh: boolean;
-  excludeFromGeneralRefresh: new BooleanField({
+  specialitiesMode: new StringField({
     nullable: false,
     required: true,
+    choices: ["one", "twoThreeFour"],
+    initial: "one",
   }),
-  // refreshesDaily: boolean;
-  refreshesDaily: new BooleanField({ nullable: false, required: true }),
-  // notes: NoteWithFormat;
-  notes: createNotesWithFormatField(),
-  // // this is defined separately for gen/inv in template.json so they have
-  // // different defaults but it's the same property
-  // hideIfZeroRated: boolean;
-  hideIfZeroRated: new BooleanField({ nullable: false, required: true }),
-  // unlocks: Unlock[];
   unlocks: new ArrayField(
     new SchemaField(
       {
-        // id: string;
-        id: new StringField({ nullable: false, required: true }),
-        // rating: number;
-        rating: new NumberField({ nullable: false, required: true }),
-        // description: string;
-        description: new StringField({ nullable: false, required: true }),
+        id: new StringField({
+          nullable: false,
+          required: true,
+        }),
+        rating: new NumberField({
+          nullable: false,
+          required: true,
+        }),
+        description: new StringField({
+          nullable: false,
+          required: true,
+        }),
       },
       {
         nullable: false,
@@ -77,27 +123,6 @@ export const createAbilitySchema = () => ({
       required: true,
     },
   ),
-  // situationalModifiers: SituationalModifier[];
-  situationalModifiers: new ArrayField(
-    new SchemaField({
-      // id: string;
-      id: new StringField({ nullable: false, required: true }),
-      // situation: string;
-      situation: new StringField({ nullable: false, required: true }),
-      // modifier: number;
-      modifier: new NumberField({ nullable: false, required: true }),
-    }),
-    {
-      nullable: false,
-      required: true,
-      initial: [],
-    },
-  ),
-  // allowPoolToExceedRating: boolean;
-  allowPoolToExceedRating: new BooleanField({
-    nullable: false,
-    required: true,
-  }),
 });
 
 export type AbilitySchema = ReturnType<typeof createAbilitySchema>;
