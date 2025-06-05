32a8222560c22bc42a6f1d716a087f7ddbb6f82a
Neil de Carteret <git@lumphammer.net>
2025-06-05 13:19:56 +0100

    refactor tinymce instantiation
    better type

--- src/components/inputs/RichTextEditor.tsx
+++ src/components/inputs/RichTextEditor.tsx
@@ -1,8 +1,16 @@
-import { useCallback, useContext, useEffect, useRef, useState } from "react";
+import {
+  RefObject,
+  useCallback,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 
 import { systemLogger, wait } from "../../functions/utilities";
 import { TextEditor } from "../../fvtt-exports";
 import { ThemeContext } from "../../themes/ThemeContext";
+import { ThemeV1 } from "../../themes/types";
 import { absoluteCover } from "../absoluteCover";
 
 type RichTextEditorProps = {
@@ -11,6 +19,54 @@ type RichTextEditorProps = {
   onSave?: () => void;
   onChange: (newSource: string) => void;
 };
+
+/** handler for making tinyMCE */
+async function makeTinyMce({
+  textAreaRef,
+  handleSave,
+  initialValue,
+  onChange,
+  theme,
+  mceRef,
+}: {
+  textAreaRef: RefObject<HTMLTextAreaElement | null>;
+  handleSave: () => void;
+  initialValue: string;
+  onChange: (newSource: string) => void;
+  theme: ThemeV1;
+  mceRef: { current: tinyMCE.Editor | null };
+}) {
+  if (!textAreaRef.current) return;
+  const mce = await TextEditor.create(
+    {
+      target: textAreaRef.current,
+      save_onsavecallback: handleSave,
+      height: "100%",
+    } as any,
+    initialValue,
+  );
+  // we know it always will be an mce (until mce gets deprecated anyway)
+  // but the types don't know that
+  if (!(mce instanceof tinyMCE.Editor)) {
+    return;
+  }
+  mce.on("change", () => {
+    const content = mce.getContent();
+    onChange(content);
+  });
+
+  // cheap hack to prevent it being times new roman
+  const style = document.createElement("style");
+  style.innerHTML = `
+          ${typeof theme.global === "string" ? theme.global : ""}
+          body {
+            --font-primary: ${theme.bodyFont};
+          }
+        `;
+  mce?.contentDocument.head.appendChild(style);
+
+  mceRef.current = mce;
+}
 
 /**
  * ham-fisted attempt to cram Foundry's TextEditor, which is itself a wrapper
@@ -24,13 +80,6 @@ export const RichTextEditor = ({
   onSave,
   onChange,
 }: RichTextEditorProps) => {
-  // useWhyDidYouUpdate("RichTextEditor", {
-  //   value,
-  //   className,
-  //   onSave,
-  //   onChange,
-  // });
-
   const handleSave = useCallback(async () => {
     // hacky delay to allow the editor to update the content before we try to
     // save it. I would try harder here but we will almost certainlky end up
@@ -40,52 +89,28 @@ export const RichTextEditor = ({
     systemLogger.log("saved");
   }, [onSave]);
 
-  const ref = useRef<HTMLTextAreaElement>(null);
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
   const [initialValue] = useState(value);
   const theme = useContext(ThemeContext);
 
   useEffect(() => {
-    let tinyMceEditor: tinyMCE.Editor | null = null;
-    if (ref.current) {
-      void TextEditor.create(
-        {
-          target: ref.current,
-          save_onsavecallback: handleSave,
-          height: "100%",
-        } as any,
-        initialValue,
-      )
-        .then((mce) => {
-          // we know it always will be an mce (until mce gets deprecated anyway)
-          // but the types don't know that
-          if (!(mce instanceof tinyMCE.Editor)) {
-            return;
-          }
-          mce.on("change", () => {
-            const content = mce.getContent();
-            onChange(content);
-          });
-          tinyMceEditor = mce;
-        })
-        .then(() => {
-          // cheap hack to prevent it being times new roman
-          const style = document.createElement("style");
-          style.innerHTML = `
-            body {
-              --font-primary: sans-serif;
-            }
-          `;
-          tinyMceEditor?.contentDocument.head.appendChild(style);
-        });
+    const mceRef: { current: tinyMCE.Editor | null } = { current: null };
+    void makeTinyMce({
+      textAreaRef,
+      handleSave,
+      initialValue,
+      onChange,
+      theme,
+      mceRef,
+    });
 
-      // effect tearddown function
-      return () => {
-        if (tinyMceEditor) {
-          tinyMceEditor.destroy();
-        }
-      };
-    }
-  }, [initialValue, handleSave, onChange]);
+    // effect tearddown function
+    return () => {
+      if (mceRef.current) {
+        mceRef.current.destroy();
+      }
+    };
+  }, [handleSave, initialValue, onChange, theme]);
 
   return (
     <form
@@ -96,7 +121,7 @@ export const RichTextEditor = ({
       }}
       className={className}
     >
-      <textarea ref={ref} css={{ height: "100%" }} />
+      <textarea ref={textAreaRef} css={{ height: "100%" }} />
     </form>
   );
 };
