7d3ecf2f39877a472ccd6654a60ec4ce56a2f179
Neil de Carteret <git@lumphammer.net>
2025-08-26 20:16:25 +0100

    classic initiative using immutable data

--- src/components/combat/CombatantRow/ClassicInitiative.tsx
+++ src/components/combat/CombatantRow/ClassicInitiative.tsx
@@ -1,4 +1,5 @@
-import { useCallback, useEffect, useRef } from "react";
+import { produce } from "immer";
+import { memo, useCallback, useEffect, useRef, useState } from "react";
 import { FaEdit, FaRecycle, FaTrash } from "react-icons/fa";
 import { HiDocumentText } from "react-icons/hi";
 
@@ -10,101 +11,129 @@ import {
   NativeMenuItem,
 } from "../../inputs/NativeMenu";
 import { NativeMenuLabel } from "../../inputs/NativeMenu/NativeMenuLabel";
+import { registerHookHandler } from "./registerHookHandler";
 import { useInititative } from "./useInititative";
 
 interface ClassicInitiativeProps {
   combatant: ClassicCombatant;
 }
 
-export const ClassicInitiative = ({ combatant }: ClassicInitiativeProps) => {
-  assertGame(game);
-  const combat = combatant.combat;
-  assertClassicCombat(combat);
-  if (combat === null) {
-    throw new Error(
-      "ClassicInitiative must be rendered with a combatant that is in combat.",
+export const ClassicInitiative = memo(
+  ({ combatant }: ClassicInitiativeProps) => {
+    assertGame(game);
+    const combat = combatant.combat;
+    assertClassicCombat(combat);
+    if (combat === null) {
+      throw new Error(
+        "ClassicInitiative must be rendered with a combatant that is in combat.",
+      );
+    }
+    const [combatantSystemData, setCombatantSystemData] = useState(
+      combatant.system.toJSON(),
     );
-  }
-  const { onConfigureCombatant, onRemoveCombatant, localize, openSheet } =
-    useInititative(combatant);
 
-  const inputRef = useRef<HTMLInputElement>(null);
-  const initString = (combatant.system.initiative ?? 0).toString();
-
-  useEffect(() => {
-    if (inputRef.current && document.activeElement !== inputRef.current) {
-      inputRef.current.value = initString;
-    }
-  }, [initString, combatant.system.initiative]);
-
-  const onResetInitiative = useCallback(() => {
-    void combatant.system.resetInitiative();
-  }, [combatant]);
-
-  const updateInitiative = () => {
-    const value = inputRef.current?.value ?? "";
-    const parsedValue = parseInt(value, 10);
-    if (isNaN(parsedValue)) {
-      return;
-    }
-    void combat.updateEmbeddedDocuments("Combatant", [
-      {
-        _id: combatant.id,
-        system: {
-          initiative: parsedValue,
+    useEffect(() => {
+      return registerHookHandler(
+        "updateCombatant",
+        (updatedCombatant, changes) => {
+          if (updatedCombatant !== combatant) return;
+          if (!("system" in changes)) return;
+          setCombatantSystemData((oldData) => {
+            const systemChanges = changes.system;
+            const newData = systemChanges
+              ? produce(oldData, (draft) => {
+                  foundry.utils.mergeObject(draft, systemChanges);
+                })
+              : oldData;
+            return newData;
+          });
         },
-      },
-    ]);
-  };
+      );
+    }, [combatant]);
 
-  return (
-    <>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          updateInitiative();
-        }}
-      >
-        <input
-          ref={inputRef}
-          type="text"
-          inputMode="numeric"
-          pattern="^[+=\-]?\d*"
-          defaultValue={initString}
-          aria-label="Initiative Score"
-          disabled={!game.user.isGM}
-          onBlur={updateInitiative}
-          css={{
-            width: "2em",
-            padding: "0.1em 0.2em",
-            height: "1.8em",
-            textAlign: "center",
-            // fontSize: "0.8em",
-            marginLeft: "0.5em",
-            marginRight: "0.5em",
+    const { onConfigureCombatant, onRemoveCombatant, localize, openSheet } =
+      useInititative(combatant);
+
+    const inputRef = useRef<HTMLInputElement>(null);
+    const initString = (combatantSystemData.initiative ?? 0).toString();
+
+    useEffect(() => {
+      if (inputRef.current && document.activeElement !== inputRef.current) {
+        inputRef.current.value = initString;
+      }
+    }, [initString]);
+
+    const onResetInitiative = useCallback(() => {
+      void combatant.system.resetInitiative();
+    }, [combatant]);
+
+    const updateInitiative = () => {
+      const value = inputRef.current?.value ?? "";
+      const parsedValue = parseInt(value, 10);
+      if (isNaN(parsedValue)) {
+        return;
+      }
+      void combat.updateEmbeddedDocuments("Combatant", [
+        {
+          _id: combatant.id,
+          system: {
+            initiative: parsedValue,
+          },
+        },
+      ]);
+    };
+
+    return (
+      <>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            updateInitiative();
           }}
-        />
-      </form>
+        >
+          <input
+            ref={inputRef}
+            type="text"
+            inputMode="numeric"
+            pattern="^[+=\-]?\d*"
+            defaultValue={initString}
+            aria-label="Initiative Score"
+            disabled={!game.user.isGM}
+            onBlur={updateInitiative}
+            css={{
+              width: "2em",
+              padding: "0.1em 0.2em",
+              height: "1.8em",
+              textAlign: "center",
+              // fontSize: "0.8em",
+              marginLeft: "0.5em",
+              marginRight: "0.5em",
+            }}
+          />
+        </form>
 
-      {game.user.isGM && (
-        <>
-          <NativeDualFunctionMenu css={{ flex: 0, padding: "0 0.3em" }}>
-            <NativeMenuLabel>{combatant.name}</NativeMenuLabel>
-            <NativeMenuItem icon={<FaEdit />} onSelect={onConfigureCombatant}>
-              {localize("COMBAT.CombatantUpdate")}
-            </NativeMenuItem>
-            <NativeMenuItem icon={<FaRecycle />} onSelect={onResetInitiative}>
-              {localize("investigator.RefreshInitiative")}
-            </NativeMenuItem>
-            <NativeMenuItem icon={<HiDocumentText />} onSelect={openSheet}>
-              {localize("investigator.OpenCharacterSheet")}
-            </NativeMenuItem>
-            <NativeMenuItem icon={<FaTrash />} onSelect={onRemoveCombatant}>
-              {localize("COMBAT.CombatantRemove")}
-            </NativeMenuItem>
-          </NativeDualFunctionMenu>
-        </>
-      )}
-    </>
-  );
-};
+        {game.user.isGM && (
+          <>
+            <NativeDualFunctionMenu css={{ flex: 0, padding: "0 0.3em" }}>
+              <NativeMenuLabel>{combatant.name}</NativeMenuLabel>
+              <NativeMenuItem icon={<FaEdit />} onSelect={onConfigureCombatant}>
+                {localize("COMBAT.CombatantUpdate")}
+              </NativeMenuItem>
+              <NativeMenuItem icon={<FaRecycle />} onSelect={onResetInitiative}>
+                {localize("investigator.RefreshInitiative")}
+              </NativeMenuItem>
+              <NativeMenuItem icon={<HiDocumentText />} onSelect={openSheet}>
+                {localize("investigator.OpenCharacterSheet")}
+              </NativeMenuItem>
+              <NativeMenuItem icon={<FaTrash />} onSelect={onRemoveCombatant}>
+                {localize("COMBAT.CombatantRemove")}
+              </NativeMenuItem>
+            </NativeDualFunctionMenu>
+          </>
+        )}
+      </>
+    );
+  },
+);
+
+ClassicInitiative.displayName = "ClassicInitiative";
