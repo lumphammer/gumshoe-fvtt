81fee35ae518dae1d4e23519b56bbabb8eaf69a6
Neil de Carteret <git@lumphammer.net>
2025-08-27 11:37:40 +0100

    break out and memoize bottom row

--- src/components/combat/CombatantRow/useCombatantData.ts
+++ src/components/combat/CombatantRow/useCombatantData.ts
@@ -54,30 +54,30 @@ export function useCombatantData(combatant: InvestigatorCombatant) {
 
   // ///////////////////////////////////////////////////////////////////////////
   // actor data
-  const [actorData, setActorData] = useState(
-    () => combatant.actor?.toJSON() ?? null,
-  );
-  useEffect(() => {
-    if (combatant.actor === null) return;
-    return registerHookHandler(
-      "updateActor",
-      (
-        updatedActor: Actor.Implementation,
-        updates: Actor.UpdateData,
-        options: Actor.Database.UpdateOptions,
-        userId: string,
-      ) => {
-        if (updatedActor.id !== combatant.actor?.id) return;
-        setActorData((previousActorData) => {
-          if (previousActorData === null) return null;
-          const newData = produce(previousActorData, (draft) => {
-            foundry.utils.mergeObject(draft, updates);
-          });
-          return newData;
-        });
-      },
-    );
-  }, [combatant]);
+  // const [actorData, setActorData] = useState(
+  //   () => combatant.actor?.toJSON() ?? null,
+  // );
+  // useEffect(() => {
+  //   if (combatant.actor === null) return;
+  //   return registerHookHandler(
+  //     "updateActor",
+  //     (
+  //       updatedActor: Actor.Implementation,
+  //       updates: Actor.UpdateData,
+  //       options: Actor.Database.UpdateOptions,
+  //       userId: string,
+  //     ) => {
+  //       if (updatedActor.id !== combatant.actor?.id) return;
+  //       setActorData((previousActorData) => {
+  //         if (previousActorData === null) return null;
+  //         const newData = produce(previousActorData, (draft) => {
+  //           foundry.utils.mergeObject(draft, updates);
+  //         });
+  //         return newData;
+  //       });
+  //     },
+  //   );
+  // }, [combatant]);
 
   // effects data
   const [effects, setEffects] = useState<
@@ -90,5 +90,5 @@ export function useCombatantData(combatant: InvestigatorCombatant) {
     });
   }, [combatant]);
 
-  return { combatantData, actorData, effects, resource };
+  return { combatantData, effects, resource };
 }
