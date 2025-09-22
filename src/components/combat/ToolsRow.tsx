56c80a2decd3bd619b31e82875e51be3ab71b67d
Neil de Carteret <git@lumphammer.net>
2025-09-22 17:04:16 +0100

    tidying

--- src/components/combat/ToolsRow.tsx
+++ src/components/combat/ToolsRow.tsx
@@ -1,18 +1,37 @@
-import { memo } from "react";
+import { memo, useCallback } from "react";
 
 import { localize } from "./functions";
+import { useTrackerContext } from "./trackerContext";
 
 export const ToolsRow = memo(function ToolsRow() {
+  const { combat } = useTrackerContext();
+  if (combat === null) {
+    throw new Error("No active combat found");
+  }
+
+  const handleSortCombatants = useCallback(() => {
+    void combat.sortCombatants();
+  }, [combat]);
+
   return (
-    <nav className="combat-controls">
+    <nav
+      className="combat-controls"
+      css={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "flex-end",
+      }}
+    >
       {" "}
       <button
         type="button"
-        className="inline-control combat-control icon fa-solid fa-chevrons-left"
-        onClick={() => null}
+        className=""
+        onClick={handleSortCombatants}
         data-tooltip=""
         aria-label={localize("COMBAT.RoundPrev")}
-      />
+      >
+        Sort combatants
+      </button>
     </nav>
   );
 });
