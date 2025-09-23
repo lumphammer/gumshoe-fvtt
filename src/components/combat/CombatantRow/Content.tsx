a01e75a8ea3d462afeb3195975fa31152de79719
Neil de Carteret <git@lumphammer.net>
2025-09-23 10:27:57 +0100

    move context menu wrapper up one level

--- src/components/combat/CombatantRow/Content.tsx
+++ src/components/combat/CombatantRow/Content.tsx
@@ -2,51 +2,44 @@ import { SyntheticListenerMap } from "@dnd-kit/core/dist/hooks/utilities";
 import { memo } from "react";
 
 import { assertGame } from "../../../functions/isGame";
-import { NativeContextMenuWrapper } from "../../inputs/NativeMenu/NativeContextMenuWrapper";
 import { BottomRow } from "./BottomRow";
 import { useCombatantContext } from "./CombatantContext";
 import { Grip } from "./Grip";
 import { TopRow } from "./TopRow";
 
 interface ContentProps {
-  setNodeRef: (node: HTMLElement | null) => void;
   setActivatorNodeRef: (node: HTMLElement | null) => void;
   listeners: SyntheticListenerMap | undefined;
 }
 
 export const Content = memo(
-  ({ setNodeRef, setActivatorNodeRef, listeners }: ContentProps) => {
+  ({ setActivatorNodeRef, listeners }: ContentProps) => {
     assertGame(game);
     const { combatantData } = useCombatantContext();
 
     return (
-      <NativeContextMenuWrapper>
-        <>
-          <Grip
-            listeners={listeners}
-            setActivatorNodeRef={setActivatorNodeRef}
-          />
-          <img
-            className="token-image"
-            src={combatantData.img || CONST.DEFAULT_TOKEN}
-            alt={combatantData.name}
-            loading="lazy"
-          />
-          <div
-            css={{
-              overflow: "hidden",
-              flex: 1,
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "space-evenly",
-              alignSelf: "stretch",
-            }}
-          >
-            <TopRow />
-            <BottomRow />
-          </div>
-        </>
-      </NativeContextMenuWrapper>
+      <>
+        <Grip listeners={listeners} setActivatorNodeRef={setActivatorNodeRef} />
+        <img
+          className="token-image"
+          src={combatantData.img || CONST.DEFAULT_TOKEN}
+          alt={combatantData.name}
+          loading="lazy"
+        />
+        <div
+          css={{
+            overflow: "hidden",
+            flex: 1,
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-evenly",
+            alignSelf: "stretch",
+          }}
+        >
+          <TopRow />
+          <BottomRow />
+        </div>
+      </>
     );
   },
 );
