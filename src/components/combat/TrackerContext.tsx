be807467f798d9b1cfc3bc2a59676abd31aa30e7
Neil de Carteret <git@lumphammer.net>
2025-09-25 16:35:13 +0100

    update state turns when combatants enter or exit

--- src/components/combat/TrackerContext.tsx
+++ src/components/combat/TrackerContext.tsx
@@ -4,6 +4,7 @@ import { createContext, useContext, useEffect, useState } from "react";
 import { assertGame } from "../../functions/isGame";
 import { systemLogger } from "../../functions/utilities";
 import { SourceData } from "../../fvtt-exports";
+import { useRefStash } from "../../hooks/useRefStash";
 import { registerHookHandler } from "./registerHookHandler";
 
 export type TrackerContextType = {
@@ -40,12 +41,26 @@ function getCombatStateFromCombat(
   };
 }
 
+function getUpdatedTurnIds(oldIds: string[], combat: Combat.Implementation) {
+  const newIds = combat.turns.map((c) => c._id).filter((id) => id !== null);
+  if (
+    newIds.length === oldIds.length &&
+    newIds.every((id, i) => id === oldIds[i])
+  ) {
+    return oldIds;
+  } else {
+    return newIds;
+  }
+}
+
 export const useTrackerContextValue = (
   combat: Combat.Implementation | null,
 ) => {
   const [combatData, setCombatData] = useState<TrackerContextType>(() => {
     return getCombatStateFromCombat(combat);
   });
+
+  const combatStash = useRefStash(combat);
 
   useEffect(() => {
     return registerHookHandler(
@@ -68,18 +83,18 @@ export const useTrackerContextValue = (
             oldData.combatState &&
             oldData.combatState._id === updatedCombat._id
           ) {
-            // this is an update to the current combat
-            const newTurnIds = updatedCombat.turns
-              .map((c) => c._id)
-              .filter((id) => id !== null);
-            const turnIds =
-              newTurnIds.length === oldData.turnIds.length &&
-              newTurnIds.every((id, i) => id === oldData.turnIds[i])
-                ? oldData.turnIds
-                : newTurnIds;
+            // const newTurnIds = updatedCombat.turns
+            //   .map((c) => c._id)
+            //   .filter((id) => id !== null);
+            // const turnIds =
+            //   newTurnIds.length === oldData.turnIds.length &&
+            //   newTurnIds.every((id, i) => id === oldData.turnIds[i])
+            //     ? oldData.turnIds
+            //     : newTurnIds;
             const combatState = produce(oldData.combatState, (draft) => {
               foundry.utils.mergeObject(draft, changes);
             });
+            const turnIds = getUpdatedTurnIds(oldData.turnIds, updatedCombat);
             const result: TrackerContextType = {
               combatState,
               combat: updatedCombat,
@@ -96,6 +111,28 @@ export const useTrackerContextValue = (
       true,
     );
   }, []);
+
+  function updateTurns(combat: Combat.Implementation | null) {
+    setCombatData((oldData) => {
+      if (combat === null || combat?.id !== oldData.combat?.id) return oldData;
+      return {
+        ...oldData,
+        turnIds: getUpdatedTurnIds(oldData.turnIds, combat),
+      };
+    });
+  }
+
+  useEffect(() => {
+    return registerHookHandler("createCombatant", (newCombatant) => {
+      updateTurns(newCombatant.combat);
+    });
+  }, []);
+
+  useEffect(() => {
+    return registerHookHandler("deleteCombatant", (deletedCombatant) => {
+      updateTurns(deletedCombatant.combat);
+    });
+  }, [combatStash]);
 
   useEffect(() => {
     return registerHookHandler("createCombat", (newCombat) => {
