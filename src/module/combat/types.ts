43aaf483078f0e16b025961b65cc00df290f0d1e
Neil de Carteret <git@lumphammer.net>
2025-12-04 19:43:41 +0000

    null is a valid combat value

--- src/module/combat/types.ts
+++ src/module/combat/types.ts
@@ -1,14 +1,16 @@
 import { isClassicCombat } from "./classicCombat";
 import { isTurnPassingCombat } from "./turnPassingCombat";
 
-export function isKnownCombat(combat: unknown): combat is Combat.Known {
+export function isKnownCombat(combat: unknown): combat is Combat.Known | null {
   // Check if the combat instance is valid
-  return isClassicCombat(combat) || isTurnPassingCombat(combat);
+  return (
+    combat === null || isClassicCombat(combat) || isTurnPassingCombat(combat)
+  );
 }
 
 export function assertKnownCombat(
   combat: unknown,
-): asserts combat is Combat.Known {
+): asserts combat is Combat.Known | null {
   if (!isKnownCombat(combat)) {
     throw new Error("Combat is not a known combat type");
   }
