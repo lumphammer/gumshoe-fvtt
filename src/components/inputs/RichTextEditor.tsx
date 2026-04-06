236217a95ada5dc66cd12ecfac49f5754ed3360f
Neil de Carteret <git@lumphammer.net>
2026-04-06 16:24:58 +0100

    new RichTextEditor

--- src/components/inputs/RichTextEditor.tsx
+++ src/components/inputs/RichTextEditor.tsx
@@ -1,129 +1,68 @@
-import {
-  RefObject,
-  useCallback,
-  useContext,
-  useEffect,
-  useRef,
-  useState,
-} from "react";
+import { useEffect, useRef } from "react";
 
-import { systemLogger, wait } from "../../functions/utilities";
-import { TextEditor } from "../../fvtt-exports";
-import { ThemeContext } from "../../themes/ThemeContext";
-import { ThemeV1 } from "../../themes/types";
-import { absoluteCover } from "../absoluteCover";
-import styles from "./RichtextEditor.css?url";
+// import styles from "./richText.module.css";
 
-type RichTextEditorProps = {
-  value: string;
-  className?: string;
-  onSave?: () => void;
-  onChange: (newSource: string) => void;
-};
-
-/** handler for making tinyMCE */
-async function makeTinyMce({
-  textAreaRef,
-  handleSave,
-  initialValue,
-  onChange,
-  theme,
-  mceRef,
-}: {
-  textAreaRef: RefObject<HTMLTextAreaElement | null>;
-  handleSave: () => void;
-  initialValue: string;
-  onChange: (newSource: string) => void;
-  theme: ThemeV1;
-  mceRef: { current: tinyMCE.Editor | null };
-}) {
-  if (!textAreaRef.current) return;
-  const mce = await TextEditor.create(
-    {
-      target: textAreaRef.current,
-      save_onsavecallback: handleSave,
-      height: "100%",
-      content_css: [styles],
-    } as any,
-    initialValue,
-  );
-  // we know it always will be an mce (until mce gets deprecated anyway)
-  // but the types don't know that
-  if (!(mce instanceof tinyMCE.Editor)) {
-    return;
-  }
-  mce.on("change", () => {
-    const content = mce.getContent();
-    onChange(content);
+export function stripStyleAttributes(html: string) {
+  const element = document.createElement("div");
+  element.innerHTML = html;
+  const styledElements = element.querySelectorAll("[style],[class]");
+  styledElements.forEach((styledElement) => {
+    styledElement.removeAttribute("style");
+    styledElement.removeAttribute("class");
   });
-
-  // cheap hack to prevent it being times new roman
-  const style = document.createElement("style");
-  style.innerHTML = `
-          ${typeof theme.global === "string" ? theme.global : ""}
-          body {
-            --font-primary: ${theme.bodyFont};
-          }
-        `;
-  mce?.contentDocument.head.appendChild(style);
-
-  mceRef.current = mce;
+  return element.innerHTML;
 }
 
-/**
- * ham-fisted attempt to cram Foundry's TextEditor, which is itself a wrapper
- * around TnyMCE, into a React component. It follows the same pattern as other
- * reacty controls in that it triggers onChange whenever the user types, and
- * calls onSave if they click the save button.
- */
 export const RichTextEditor = ({
-  value,
-  className,
-  onSave,
+  html,
   onChange,
-}: RichTextEditorProps) => {
-  const handleSave = useCallback(async () => {
-    // hacky delay to allow the editor to update the content before we try to
-    // save it. I would try harder here but we will almost certainlky end up
-    // using foundry's cool new editor in due course anyway.
-    await wait(500);
-    onSave?.();
-    systemLogger.log("saved");
-  }, [onSave]);
-
-  const textAreaRef = useRef<HTMLTextAreaElement>(null);
-  const [initialValue] = useState(value);
-  const theme = useContext(ThemeContext);
+  documentUUID,
+  className,
+}: {
+  className: string | undefined;
+  html: string;
+  onChange: (html: string) => void;
+  documentUUID: string;
+}) => {
+  // const [html, setHtml] = createSignal(props.initialHtml);
+  const divRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    const mceRef: { current: tinyMCE.Editor | null } = { current: null };
-    void makeTinyMce({
-      textAreaRef,
-      handleSave,
-      initialValue,
-      onChange,
-      theme,
-      mceRef,
+    // borrowed lovingly from https://github.com/asacolips-projects/13th-age/blob/acd49c1d8b1eaf63f544c1d2e5a4aa1a74742c9e/src/module/item/power-sheet-v2.js#L148
+    const editor = foundry.applications.elements.HTMLProseMirrorElement.create({
+      toggled: true,
+      // collaborate: true,
+      documentUUID: documentUUID,
+      enriched: html,
+      compact: true,
+      name: "system.text",
+      value: html,
     });
-
-    // effect tearddown function
+    editor.addEventListener("change", () => {
+      console.log("change", editor.value);
+      const strippedHTML = stripStyleAttributes(editor.value);
+      console.log("strippedHTML", strippedHTML);
+      onChange(strippedHTML);
+    });
+    divRef.current?.appendChild(editor);
+    const div = divRef.current;
     return () => {
-      if (mceRef.current) {
-        mceRef.current.destroy();
-      }
+      div?.removeChild(editor);
+      editor.remove();
     };
-  }, [handleSave, initialValue, onChange, theme]);
+  }, [documentUUID, html, onChange]);
 
   return (
-    <form
+    <div
+      ref={divRef}
+      // className={`absolute inset-0 my-2 ${styles["rich-text-editor"]}`}
       css={{
-        ...absoluteCover,
-        backgroundColor: "white",
-        "--font-primary": theme.bodyFont,
+        position: "absolute",
+        inset: "2px",
+        marginLeft: "0.5em",
+        marginRight: "0.5em",
       }}
-      className={className}
-    >
-      <textarea ref={textAreaRef} css={{ height: "100%" }} />
-    </form>
+      className={`${className} `}
+    />
   );
 };
